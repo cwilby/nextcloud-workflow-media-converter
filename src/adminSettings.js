8e97a339faf38eed8a54821f5b50ae175abe155a
Cameron Wilby <cameron@wilby.com>
2024-03-31 09:56:26 -0700

    Propagate nonce to admin app

--- src/adminSettings.js
+++ src/adminSettings.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import './bootstrap.js'
 import AdminSettings from './views/AdminSettings.vue'
 
+// CSP config for webpack dynamic chunk loading
+// eslint-disable-next-line
+__webpack_nonce__ = btoa(OC.requestToken)
+
 new Vue({ // eslint-disable-line no-new
 	el: '#workflow_media_converter-adminSettings',
 	render: h => h(AdminSettings),
