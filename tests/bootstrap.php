1d821ec6cfa920a81a353c2e0d61f2ea19335827
Cameron Wilby <cameron@wilby.com>
2026-01-17 23:07:31 -0800

    Update tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,14 +20,15 @@
  *
  */
 
+use OCP\App\IAppManager;
+use OCP\Server;
+
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 	define('USEDOCKER', 1);
 }
+
 require_once __DIR__ . '/../../../lib/base.php';
-\OC::$loader->addValidRoot(\OC::$SERVERROOT . '/tests');
-\OC_App::loadApp('workflow_media_converter');
-if (!class_exists('\PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
-OC_Hook::clear();
+require_once __DIR__ . '/../../../../tests/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('workflow_media_converter');
